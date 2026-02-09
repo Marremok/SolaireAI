a80370b6f464f2c31b7aaf0d57d2b2581bdf862b
unknown <maxnylander@icloud.com>
2026-02-09 21:30:37 +0100

    page.tsx blunder AGAIN

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,13 +25,13 @@ export const metadata: Metadata = {
 };
 
 export default async function Home() {
-  // const user = await currentUser()
+  const user = await currentUser()
   
   // syncing webhooks
-  // await syncUser();
+  await syncUser();
 
-  // redircet auth user to dashboard
-  // if (user) redirect("/dashboard")
+  //redircet auth user to dashboard
+  if (user) redirect("/dashboard")
 
   return (
   
